40d6f14e7555ecc36e1aa128d876fb6ca755fa7c
CAPTAIN-SONOSHEE <coulibaly-ablo@hotmail.fr>
2023-09-02 18:24:00 +0200

    adding high score features

--- scoreboard.py
+++ scoreboard.py
@@ -7,6 +7,8 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read()) 
         self.color("white")
         self.penup()
         self.hideturtle()
@@ -20,8 +22,14 @@ class ScoreBoard(Turtle):
 
     def refresh_board(self):
          self.clear()
-         self.write(f"Score : {self.score}", align= ALIGN, font= FONT)    
-
+         self.write(f"Score : {self.score} High score : {self.high_score}", align= ALIGN, font= FONT)    
+         
     def game_over(self):
-        self.goto(0,0)
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                    data.write(f"{self.high_score}")
+
+        self.home()
         self.write(f"GAME OVER", align= ALIGN, font= FONT) 
+        
